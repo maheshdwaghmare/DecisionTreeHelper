a5ff59257b4c1481d8c2726d5c24b9c7aa038824
Mahesh ED <Mahesh.Waghmare@Effective.Digital>
2020-01-21 12:15:27 +0530

    Added Project to Git repo.

---
+++ DecisionTreeHelper/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DecisionTreeHelper.Models;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DecisionTreeHelper.Controllers
+{
+    public class Result
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string Error { get; set; }
+    }
+
+
+    public class Question
+    {
+        public string QuestionID { get; set; }
+        public string QuestionStatement { get; set; }
+        public string ParentQuestionID { get; set; }
+        public bool Answer { get; set; }
+        public List<Question> questions { get; set; }
+    }
+    
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public void SubmitResult(string result)
+        {
+            //save json searlize data of questions and answers in result.txt file
+            System.IO.File.WriteAllText(@"result.txt", result);
+        }
+
+        [HttpGet]
+        public List<Result> GetDecisionTree()
+        {
+            List<Result> result = new List<Result>();
+            try
+            {
+                string fileContents = null;
+                using (StreamReader sr = new StreamReader(@"result.txt"))
+                {
+                    string line = sr.ReadToEnd();
+                    fileContents += line;
+                }
+
+                result = JsonConvert.DeserializeObject<List<Result>>(fileContents);
+            }
+            catch (FileNotFoundException)
+            {
+                result.Add(new Result { Error = "Result not found. Please start a Quiz!" });
+            }
+            return result;
+        }
+    }
+}
---
+++ DecisionTreeHelper/wwwroot/wwwroot/js/app.js
@@ -0,0 +1,221 @@
+﻿var app = angular.module('quizApp', []);
+    app.directive('quiz', function (quizFactory) {
+        return {
+            restrict: 'AE',
+            scope: {},
+            templateUrl: 'template.html',
+            link: function (scope, elem, attrs) {
+                scope.start = function () {
+
+                    // reset all values and fetch first question object.
+                    scope.id = 0;
+                    scope.choices = "";
+                    scope.tree = "";
+                    scope.question = [];
+                    scope.results = [];
+                    scope.quizOver = false;
+                    scope.inProgress = true;
+                    scope.questionNo = 1;
+                    scope.Error = "";
+                    scope.getQuestionById("root", "Question1");
+                };
+                scope.reset = function () {
+
+                    // reset all values
+                    scope.quizOver = false;
+                    scope.inProgress = false;
+                    scope.choices = "";
+                    scope.tree = "";
+                    scope.Error = "";
+                    scope.results = [];
+                    scope.question = [];
+                    questionNo = 1;
+                };
+                scope.display = function () {
+                    scope.decisiontree = [];
+
+                    $.ajax({
+                        url: '/Home/GetDecisionTree',
+                        success: function (response) {
+                            scope.results = response;
+                            if (scope.results[0].error) {
+                                scope.Error = scope.results[0].error;
+                            }
+                            else {
+                                scope.Error = "";
+                                for (var i = 0; i < scope.results.length; i++) {
+                                    var question = scope.results[i];
+                                    scope.object = {
+                                        'id': (question.answer) ? "Question : " + (i + 1) : "",
+                                        'question': question.question,
+                                        'answer': question.answer
+                                    };
+
+                                    scope.decisiontree.push(scope.object);
+                                }
+                            }
+                        },
+                        async: false
+                    });
+                };
+
+                scope.getQuestionById = function (parentid, val) {
+                    var q = quizFactory.getQuestionById(parentid, val);
+                    if (q.options) {
+
+                        // Reset all radio buttons
+                        for (var i = 0; i < q.options.length; i++) {
+                            if (q.options[i].selectedRadio) {
+                                q.options[i].selectedRadio = "";
+                            }
+                        }
+
+                        // create new object of newly fetched question details
+                        scope.object = {
+                            'parentQuestionId': q.parentQuestionId,
+                            'questionId': q.questionId,
+                            'question': q.question,
+                            'options': q.options
+                        };
+                        
+                        scope.question.push(scope.object);
+                    } else {
+
+                        // Display final outcome
+                        scope.quizOver = true;
+                        scope.choices += "\n" + "Final Answer: " + q.question;
+
+                        // Save decision tree in result.txt file
+                        scope.result = { 'Question': q.question };
+                        scope.results.push(scope.result);
+                        var results = angular.toJson(scope.results);
+                        $.post('/Home/SubmitResult', { 'result': results }).then(function (response) {
+                        });
+                    }
+                };
+
+                scope.checkAnswer = function (question, val) {
+
+                    // check answer of the question and call next question based on current answer
+                    var options = question.options;
+                    var ans = "";
+                    for (var i = 0; i < options.length; i++) {
+                        if (options[i].selectedRadio) {
+                            ans = options[i].selectedRadio;
+                        }
+                    }
+
+                    // push changes to resault results object array for saving in results.txt file
+                    if (ans) {
+                        scope.result = { 'Question': question.question, 'Answer': ans };
+                        scope.results.push(scope.result);
+                    }
+
+                    // display questions along with choices
+                    scope.choices += "\n" + scope.questionNo + ". Question: " + question.question + "\n";
+                    scope.choices += "Answer: " + ans + "\n";
+                    scope.questionNo++;
+
+                    // get next question by questionId
+                    scope.getQuestionById(question.questionId, val);
+                };
+            }
+        };
+    });
+
+app.factory('quizFactory', function () {
+    var questions = [{
+        parentQuestionId: "root",
+        questionId: "Question1",
+        question: "Do I want a Doughnut?",
+        options: [{
+            name: "Yes",
+            selected: false
+        }, {
+            name: "No",
+            selected: false
+        }]
+    }, {
+        parentQuestionId: "Question1Yes",
+        questionId: "Question2",
+        question: "Do I deserve it?",
+        options: [{
+            name: "Yes",
+            selected: false
+        }, {
+            name: "No",
+            selected: false
+        }]
+    },
+    {
+        parentQuestionId: "Question1No",
+        questionId: "Question3",
+        question: "Maybe you want an apple?"
+    },
+    {
+        parentQuestionId: "Question2Yes",
+        questionId: "Question4",
+        question: "Are you sure?",
+        options: [{
+            name: "Yes",
+            selected: false
+        }, {
+            name: "No",
+            selected: false
+        }]
+    },
+    {
+        parentQuestionId: "Question2No",
+        questionId: "Question5",
+        question: "Is it a good doughnut?",
+        options: [{
+            name: "Yes",
+            selected: false
+        }, {
+            name: "No",
+            selected: false
+        }]
+    },
+    {
+        parentQuestionId: "Question4Yes",
+        questionId: "Question6",
+        question: "Get it?"
+    },
+    {
+        parentQuestionId: "Question4No",
+        questionId: "Question7",
+        question: "Do jumping jacks first?"
+    },
+    {
+        parentQuestionId: "Question5Yes",
+        questionId: "Question8",
+        question: "What are you waiting for? Grab it now."
+    },
+    {
+        parentQuestionId: "Question5No",
+        questionId: "Question9",
+        question: "Wait till you find a sinful, unforgettable doughnut."
+    }];
+
+    return {
+        getQuestion: function (id) {
+            if (id < questions.length) {
+                return questions[id];
+            } else {
+                return false;
+            }
+        },
+        updateQuestion: function (id, ans) {
+            questions[id].answers = ans;
+        },
+        getQuestionById: function (question, val) {
+            if (question === "root") {
+                parentid = question;
+                var selectedQuestion = questions.filter(i => i.parentQuestionId === parentid)[0];
+                return selectedQuestion;
+            }
+            parentid = question + val;
+            return questions.filter(i => i.parentQuestionId === parentid)[0];
+        }
+    };
+});
